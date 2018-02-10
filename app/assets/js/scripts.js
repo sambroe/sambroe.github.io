ef35121665d4aba14dfba0b199215780ca8ca171
Sam Broe <sjbroe@gmail.com>
2018-02-10 00:02:24 -0500

    Setting up new project

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * boilerplate
+ * 
+ * 
+ * @author Sam Broe
+ * @version 1.0.0
+ * Copyright 2018. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+
+  });
+
+})(jQuery, window, document);
